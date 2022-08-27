01926e44976b7caa1f877633248c7ce420c70db9
Lunarmagpie <bambolambo0@gmail.com>
2022-08-27 10:43:32 -0400

    format fix

--- tests/toolbox/test_as_command_choices.py
+++ tests/toolbox/test_as_command_choices.py
@@ -12,6 +12,7 @@ def same_name_and_value():
         hikari.CommandChoice(name="c", value="c"),
     )
 
+
 @pytest.fixture
 def diff_name_and_value():
     return (
@@ -20,11 +21,14 @@ def diff_name_and_value():
         hikari.CommandChoice(name="c", value="f"),
     )
 
+
 def test_sequence_choices(same_name_and_value):
     assert toolbox.as_command_choices(["a", "b", "c"]) == same_name_and_value
 
+
 def test_sequence_sequence_choices(diff_name_and_value):
     assert toolbox.as_command_choices([["a", "d"], ["b", "e"], ["c", "f"]]) == diff_name_and_value
+
 
 def test_dict_choices(diff_name_and_value):
     assert (
@@ -38,11 +42,14 @@ def test_dict_choices(diff_name_and_value):
         == diff_name_and_value
     )
 
+
 def test_vargs_choices(same_name_and_value):
     assert toolbox.as_command_choices("a", "b", "c") == same_name_and_value
 
+
 def test_vargs_sequence_choices(diff_name_and_value):
     assert toolbox.as_command_choices(["a", "d"], ["b", "e"], ["c", "f"]) == diff_name_and_value
+
 
 def test_kwargs_choices(diff_name_and_value):
     toolbox.as_command_choices(
