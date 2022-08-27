3d4424ebb195f5aa6c4b6fe81c320a88fcda5aa6
Lunarmagpie <bambolambo0@gmail.com>
2022-08-27 10:31:39 -0400

    remove class

--- tests/toolbox/test_as_command_choices.py
+++ tests/toolbox/test_as_command_choices.py
@@ -4,50 +4,49 @@ import pytest
 import toolbox
 
 
-class TestAsCommandChoices:
-    @pytest.fixture
-    def same_name_and_value(self):
-        return (
-            hikari.CommandChoice(name="a", value="a"),
-            hikari.CommandChoice(name="b", value="b"),
-            hikari.CommandChoice(name="c", value="c"),
-        )
+@pytest.fixture
+def same_name_and_value(self):
+    return (
+        hikari.CommandChoice(name="a", value="a"),
+        hikari.CommandChoice(name="b", value="b"),
+        hikari.CommandChoice(name="c", value="c"),
+    )
 
-    @pytest.fixture
-    def diff_name_and_value(self):
-        return (
-            hikari.CommandChoice(name="a", value="d"),
-            hikari.CommandChoice(name="b", value="e"),
-            hikari.CommandChoice(name="c", value="f"),
-        )
+@pytest.fixture
+def diff_name_and_value(self):
+    return (
+        hikari.CommandChoice(name="a", value="d"),
+        hikari.CommandChoice(name="b", value="e"),
+        hikari.CommandChoice(name="c", value="f"),
+    )
 
-    def test_sequence_choices(self, same_name_and_value):
-        assert toolbox.as_command_choices(["a", "b", "c"]) == same_name_and_value
+def test_sequence_choices(self, same_name_and_value):
+    assert toolbox.as_command_choices(["a", "b", "c"]) == same_name_and_value
 
-    def test_sequence_sequence_choices(self, diff_name_and_value):
-        assert toolbox.as_command_choices([["a", "d"], ["b", "e"], ["c", "f"]]) == diff_name_and_value
+def test_sequence_sequence_choices(self, diff_name_and_value):
+    assert toolbox.as_command_choices([["a", "d"], ["b", "e"], ["c", "f"]]) == diff_name_and_value
 
-    def test_dict_choices(self, diff_name_and_value):
-        assert (
-            toolbox.as_command_choices(
-                {
-                    "a": "d",
-                    "b": "e",
-                    "c": "f",
-                }
-            )
-            == diff_name_and_value
-        )
-
-    def test_vargs_choices(self, same_name_and_value):
-        assert toolbox.as_command_choices("a", "b", "c") == same_name_and_value
-
-    def test_vargs_sequence_choices(self, diff_name_and_value):
-        assert toolbox.as_command_choices(["a", "d"], ["b", "e"], ["c", "f"]) == diff_name_and_value
-
-    def test_kwargs_choices(self, diff_name_and_value):
+def test_dict_choices(self, diff_name_and_value):
+    assert (
         toolbox.as_command_choices(
-            a="d",
-            b="e",
-            c="f",
-        ) == diff_name_and_value
+            {
+                "a": "d",
+                "b": "e",
+                "c": "f",
+            }
+        )
+        == diff_name_and_value
+    )
+
+def test_vargs_choices(self, same_name_and_value):
+    assert toolbox.as_command_choices("a", "b", "c") == same_name_and_value
+
+def test_vargs_sequence_choices(self, diff_name_and_value):
+    assert toolbox.as_command_choices(["a", "d"], ["b", "e"], ["c", "f"]) == diff_name_and_value
+
+def test_kwargs_choices(self, diff_name_and_value):
+    toolbox.as_command_choices(
+        a="d",
+        b="e",
+        c="f",
+    ) == diff_name_and_value
