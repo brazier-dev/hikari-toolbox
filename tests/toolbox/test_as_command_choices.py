eb8ad6fb9cbb28e4aae937bf92f252e1b9b48c93
Lunarmagpie <bambolambo0@gmail.com>
2022-08-27 10:38:07 -0400

    remove self in tests

--- tests/toolbox/test_as_command_choices.py
+++ tests/toolbox/test_as_command_choices.py
@@ -5,7 +5,7 @@ import toolbox
 
 
 @pytest.fixture
-def same_name_and_value(self):
+def same_name_and_value():
     return (
         hikari.CommandChoice(name="a", value="a"),
         hikari.CommandChoice(name="b", value="b"),
@@ -13,20 +13,20 @@ def same_name_and_value(self):
     )
 
 @pytest.fixture
-def diff_name_and_value(self):
+def diff_name_and_value():
     return (
         hikari.CommandChoice(name="a", value="d"),
         hikari.CommandChoice(name="b", value="e"),
         hikari.CommandChoice(name="c", value="f"),
     )
 
-def test_sequence_choices(self, same_name_and_value):
+def test_sequence_choices(same_name_and_value):
     assert toolbox.as_command_choices(["a", "b", "c"]) == same_name_and_value
 
-def test_sequence_sequence_choices(self, diff_name_and_value):
+def test_sequence_sequence_choices(diff_name_and_value):
     assert toolbox.as_command_choices([["a", "d"], ["b", "e"], ["c", "f"]]) == diff_name_and_value
 
-def test_dict_choices(self, diff_name_and_value):
+def test_dict_choices(diff_name_and_value):
     assert (
         toolbox.as_command_choices(
             {
@@ -38,13 +38,13 @@ def test_dict_choices(self, diff_name_and_value):
         == diff_name_and_value
     )
 
-def test_vargs_choices(self, same_name_and_value):
+def test_vargs_choices(same_name_and_value):
     assert toolbox.as_command_choices("a", "b", "c") == same_name_and_value
 
-def test_vargs_sequence_choices(self, diff_name_and_value):
+def test_vargs_sequence_choices(diff_name_and_value):
     assert toolbox.as_command_choices(["a", "d"], ["b", "e"], ["c", "f"]) == diff_name_and_value
 
-def test_kwargs_choices(self, diff_name_and_value):
+def test_kwargs_choices(diff_name_and_value):
     toolbox.as_command_choices(
         a="d",
         b="e",
