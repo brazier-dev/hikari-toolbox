1e77ee5f468d8578768017bcbf950ed5e351f037
Labret <jfr.labret@hotmail.com>
2022-09-18 19:33:45 +0200

    added test_remove_bold, renamed formats

--- tests/toolbox/test_remove_markdown.py
+++ tests/toolbox/test_remove_markdown.py
@@ -1,4 +1,4 @@
-from toolbox import remove_strikethrough, remove_block, remove_multiblock
+from toolbox import remove_strikethrough, remove_code_block, remove_multi_code_block, remove_bold
 
 
 def test_remove_strikethrough():
@@ -18,32 +18,48 @@ def test_remove_strikethrough():
     assert remove_strikethrough("~~test \nwith line break~~") == "test \nwith line break"
 
 
-def test_remove_block():
-    assert remove_block("`test`") == "test"
-    assert remove_block("test") == "test"
-    assert remove_block("`test") == "`test"
-    assert remove_block("`test` `another test`") == "test another test"
-    assert remove_block("`test` another test`") == "test another test`"
-    assert remove_block("`test` `another test` `and another one`") == "test another test and another one"
-    assert remove_block("`test``") == "test`"
-    assert remove_block("test  ``more randomness`") == "test  more randomness`"
-    assert remove_block("test  ` `more randomness`") == "test   more randomness`"
+def test_remove_code_block():
+    assert remove_code_block("`test`") == "test"
+    assert remove_code_block("test") == "test"
+    assert remove_code_block("`test") == "`test"
+    assert remove_code_block("`test` `another test`") == "test another test"
+    assert remove_code_block("`test` another test`") == "test another test`"
+    assert remove_code_block("`test` `another test` `and another one`") == "test another test and another one"
+    assert remove_code_block("`test``") == "test`"
+    assert remove_code_block("test  ``more randomness`") == "test  more randomness`"
+    assert remove_code_block("test  ` `more randomness`") == "test   more randomness`"
     assert (
-        remove_block("`test` we write some random` gibberish `hehe `")
+        remove_code_block("`test` we write some random` gibberish `hehe `")
         == "test we write some random gibberish hehe `"
     )
-    assert remove_block("`test \nwith line break`") == "test \nwith line break"
+    assert remove_code_block("`test \nwith line break`") == "test \nwith line break"
 
-def test_remove_multiblock():
-    assert remove_multiblock("```test```") == "test"
-    assert remove_multiblock("test") == "test"
-    assert remove_multiblock("```test``` ```another test```") == "test another test"
-    assert remove_multiblock("```test``` another test`") == "test another test`"
-    assert remove_multiblock("```test``` ```another test``` ```and another one```") == "test another test and another one"
-    assert remove_multiblock("```test````") == "test`"
-    assert remove_multiblock("test  ``` ```more randomness`") == "test   more randomness`"
+def test_remove_multi_code_block():
+    assert remove_multi_code_block("```test```") == "test"
+    assert remove_multi_code_block("test") == "test"
+    assert remove_multi_code_block("```test``` ```another test```") == "test another test"
+    assert remove_multi_code_block("```test``` another test`") == "test another test`"
+    assert remove_multi_code_block("```test``` ```another test``` ```and another one```") == "test another test and another one"
+    assert remove_multi_code_block("```test````") == "test`"
+    assert remove_multi_code_block("test  ``` ```more randomness`") == "test   more randomness`"
     assert (
-        remove_multiblock("```test``` we write some random` gibberish ```hehe ```")
+        remove_multi_code_block("```test``` we write some random` gibberish ```hehe ```")
         == "test we write some random` gibberish hehe "
     )
-    assert remove_multiblock("```test \nwith line break```") == "test \nwith line break"
+    assert remove_multi_code_block("```test \nwith line break```") == "test \nwith line break"
+
+def test_remove_bold():
+    assert remove_bold("**test**") == "test"
+    assert remove_bold("test") == "test"
+    assert remove_bold("**test") == "**test"
+    assert remove_bold("**test** **another test**") == "test another test"
+    assert remove_bold("**test** another test**") == "test another test**"
+    assert remove_bold("**test** **another test** **and another one**") == "test another test and another one"
+    assert remove_bold("**test***") == "test*"
+    assert remove_bold("test  ***more randomness**") == "test  *more randomness"
+    assert remove_bold("test  ** **more randomness**") == "test   more randomness**"
+    assert (
+        remove_bold("**test** we write some random** gibberish **hehe **")
+        == "test we write some random gibberish hehe **"
+    )
+    assert remove_bold("**test \nwith line break**") == "test \nwith line break"
