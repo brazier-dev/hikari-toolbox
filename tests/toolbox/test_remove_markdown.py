885abfd064567ca2e5794799b381899765aaa7dd
Labret <jfr.labret@hotmail.com>
2022-09-18 19:02:14 +0200

    added test_remove_multiblock

--- tests/toolbox/test_remove_markdown.py
+++ tests/toolbox/test_remove_markdown.py
@@ -1,4 +1,4 @@
-from toolbox import remove_strikethrough, remove_block
+from toolbox import remove_strikethrough, remove_block, remove_multiblock
 
 
 def test_remove_strikethrough():
@@ -33,3 +33,17 @@ def test_remove_block():
         == "test we write some random gibberish hehe `"
     )
     assert remove_block("`test \nwith line break`") == "test \nwith line break"
+
+def test_remove_multiblock():
+    assert remove_multiblock("```test```") == "test"
+    assert remove_multiblock("test") == "test"
+    assert remove_multiblock("```test``` ```another test```") == "test another test"
+    assert remove_multiblock("```test``` another test`") == "test another test`"
+    assert remove_multiblock("```test``` ```another test``` ```and another one```") == "test another test and another one"
+    assert remove_multiblock("```test````") == "test`"
+    assert remove_multiblock("test  ``` ```more randomness`") == "test   more randomness`"
+    assert (
+        remove_multiblock("```test``` we write some random` gibberish ```hehe ```")
+        == "test we write some random` gibberish hehe "
+    )
+    assert remove_multiblock("```test \nwith line break```") == "test \nwith line break"
