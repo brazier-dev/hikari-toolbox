414898d83608825e8e52e9c664f5a57b5a715f75
Labret <jfr.labret@hotmail.com>
2022-09-18 14:36:09 +0200

    more tests

--- tests/toolbox/test_remove_md.py
+++ tests/toolbox/test_remove_md.py
@@ -3,9 +3,16 @@ from toolbox import remove_strikethrough
 
 def test_remove_strikethrough():
     assert remove_strikethrough("~~test~~") == "test"
+    assert remove_strikethrough("~test~") == "~test~"
     assert remove_strikethrough("test") == "test"
     assert remove_strikethrough("~~test") == "~~test"
     assert remove_strikethrough("~~test~~ ~~another test~~") == "test another test"
     assert remove_strikethrough("~~test~~ another test~~") == "test another test~~"
+    assert remove_strikethrough("~~test~~ ~~another test~~ ~~and another one~~") == "test another test and another one"
     assert remove_strikethrough("~~test~~~") == "test~"
-    assert remove_strikethrough("~~test~~ we write some random~ gibberish ~~hehe ~~") == "test we write some random~ gibberish hehe "
+    assert remove_strikethrough("test  ~~~more randomness~~") == "test  ~more randomness"
+    assert (
+        remove_strikethrough("~~test~~ we write some random~ gibberish ~~hehe ~~")
+        == "test we write some random~ gibberish hehe "
+    )
+    assert remove_strikethrough("~~test \nwith line break~~") == "test \nwith line break"
