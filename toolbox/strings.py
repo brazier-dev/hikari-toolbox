56fd809a14bd6de60251964f673d448433d13700
Labret <jfr.labret@hotmail.com>
2022-09-24 22:31:04 +0200

    removed subfunctions, build general function

--- toolbox/strings.py
+++ toolbox/strings.py
@@ -9,17 +9,8 @@ __all__: t.Sequence[str] = (
     "is_url",
     "is_invite",
     "remove_markdown",
-    "remove_strikethrough",
-    "remove_code_blocks",
-    "remove_multi_code_blocks",
-    "remove_bold",
-    "remove_underlines",
-    "remove_underscore_italics",
-    "remove_asterisk_italics",
-    "remove_spoilers",
-    "remove_quotes",
-    "remove_multi_quotes",
-    )
+    "MarkdownFormat",
+)
 
 VALID_TIMESTAMP_STYLES: t.Sequence[str] = ("t", "T", "d", "D", "f", "F", "R")
 
@@ -118,35 +109,48 @@ def is_invite(string: str, *, fullmatch: bool = True) -> bool:
 
     return False
 
-STRIKETHROUGH_REGEX = re.compile(r"~~([\S\s]*?)~~")
-ITALIC_UNDERSCORE_REGEX = re.compile(r"_([^_]+)_")
-ITALIC_ASTERISK_REGEX = re.compile(r"\*([^*]+)\*")
-BOLD_REGEX = re.compile(r"\*{2}([\s\S]*?)\*{2}")
-UNDERLINE_REGEX = re.compile(r"__([\s\S]*?)__")
-SPOILER_REGEX = re.compile(r"\|{2}([\s\S]+?)\|{2}")
-CODE_BLOCK_REGEX = re.compile(r"`([^`]+)`")
-MULTI_CODE_BLOCK_REGEX = re.compile(r"`{3}([\S\s]*?)`{3}")
-QUOTE_REGEX = re.compile(r"\s*\> (.*)", re.DOTALL)
-MULTI_QUOTE_REGEX = re.compile(r"\s*\>>> (.*)", re.DOTALL)
-
 
 class MarkdownFormat(IntFlag):
     NONE = 0
     STRIKETHROUGH = 1
-    ITALIC = 2
-    BOLD = 4
-    UNDERLINE = 8
-    CODE_BLOCK = 16
-    MULTI_CODE_BLOCK = 32
-    QUOTE = 64
-    MULTI_QUOTE = 128
-    SPOILER = 256
-    ALL = STRIKETHROUGH | ITALIC | BOLD | UNDERLINE | CODE_BLOCK | MULTI_CODE_BLOCK | QUOTE | MULTI_QUOTE | SPOILER
+    ITALIC_UNDERSCORE = 2
+    ITALIC_ASTERISK = 4
+    BOLD = 8
+    UNDERLINE = 16
+    CODE_BLOCK = 32
+    MULTI_CODE_BLOCK = 64
+    QUOTE = 128
+    MULTI_QUOTE = 256
+    SPOILER = 512
+    ALL = (
+        STRIKETHROUGH
+        | ITALIC_UNDERSCORE
+        | ITALIC_ASTERISK
+        | BOLD
+        | UNDERLINE
+        | CODE_BLOCK
+        | MULTI_CODE_BLOCK
+        | QUOTE
+        | MULTI_QUOTE
+        | SPOILER
+    )
 
 
-def remove_markdown(content: str, 
-formats: MarkdownFormat = MarkdownFormat.ALL
-) -> str:
+format_dict = {
+    MarkdownFormat.MULTI_QUOTE: [r"\s*\>>> ([\s\S]+?)", ">>> {0}"],
+    MarkdownFormat.QUOTE: [r"\s*\> ([\s\S]+?)", "> {0}"],
+    MarkdownFormat.MULTI_CODE_BLOCK: [r"`{3}([\S\s]+?)`{3}", "```{0}```"],
+    MarkdownFormat.CODE_BLOCK: [r"`([^`]+?)`", "`{0}`"],
+    MarkdownFormat.BOLD: [r"\*{2}([\s\S]+?)\*{2}", "\*\*{0}\*\*"],
+    MarkdownFormat.UNDERLINE: [r"__([\s\S]+?)__", "__{0}__"],
+    MarkdownFormat.STRIKETHROUGH: [r"~~([\S\s]+?)~~", "~~{0}~~"],
+    MarkdownFormat.ITALIC_UNDERSCORE: [r"_([^_]+?)_", "_{0}_"],
+    MarkdownFormat.ITALIC_ASTERISK: [r"\*([^*]+?)\*", "\*{0}\*"],
+    MarkdownFormat.SPOILER: [r"\|{2}([\s\S]+?)\|{2}", "\|\|{0}\|\|"],
+}
+
+
+def remove_markdown(content: str, formats: MarkdownFormat = MarkdownFormat.ALL) -> str:
     """Removes the markdown formatting from Discord messages.
 
     Parameters
@@ -154,7 +158,7 @@ formats: MarkdownFormat = MarkdownFormat.ALL
     content : str
         The `str` object, which needs their content cleaned from Discord's markdown formatting.
     formats : MarkdownFormat
-        The `IntFlag` of the kind of formatting that needs to be removed. Default is ALL.
+        The `IntFlag` the of formatting that needs to be removed. Default is ALL.
 
     Returns
     -------
@@ -163,239 +167,25 @@ formats: MarkdownFormat = MarkdownFormat.ALL
     """
     if content is None:
         return "Message is empty"
-    if (formats & MarkdownFormat.ALL):
-        content = remove_spoilers(content)
-        content = remove_multi_quotes(content)
-        content = remove_quotes(content)
-        content = remove_multi_code_blocks(content)
-        content = remove_code_blocks(content)
-        content = remove_underlines(content)
-        content = remove_bold(content)
-        content = remove_asterisk_italics(content)
-        content = remove_underscore_italics(content)
-        content = remove_strikethrough(content)
-        return content
+
+    match_list = []
+    for format in format_dict:
+        if formats & format:
+            search = re.compile(format_dict[format][0])
+            match_list.append(re.findall(search, content))
+    for format in format_dict:
+        if formats & format:
+            for matches in match_list:
+                if not matches:
+                    continue
+                print(matches)
+                for match in matches:
+                    content = re.sub(format_dict[format][1].format(match), match, content)
+                    print(match)
+    print(f"Cleaned: {content}")
+    return content
 
 
-def remove_strikethrough(content: str) -> str:
-    """Removes the strikethrough formatting from Discord messages.
-
-    Parameters
-    ----------
-    content : str
-        The `str` object to be cleaned from strikethrough.
-
-    Returns
-    -------
-    str
-        The cleaned string without strikethrough formatting.
-    """
-    matches = re.findall(STRIKETHROUGH_REGEX, content)
-    if not matches:
-        return content
-    cleaned = re.sub(f"~~{matches[0]}~~", matches[0], content)
-    for match in matches:
-        cleaned = re.sub(f"~~{match}~~", match, cleaned)
-    return cleaned
-
-
-def remove_code_blocks(content: str) -> str:
-    """Removes the code block formatting from Discord messages.
-
-    Parameters
-    ----------
-    content : str
-        The `str` object to be cleaned from code block formatting.
-
-    Returns
-    -------
-    str
-        The cleaned string without code block formatting.
-    """
-    matches = re.findall(CODE_BLOCK_REGEX, content)
-    if not matches:
-        return content
-    cleaned = re.sub(f"`{matches[0]}`", matches[0], content)
-    for match in matches:
-        cleaned = re.sub(f"`{match}`", match, cleaned)
-    return cleaned
-
-
-def remove_multi_code_blocks(content: str) -> str:
-    """Removes the multiline codeblock formatting from Discord messages.
-
-    Parameters
-    ----------
-    content : str
-        The `str` object to be cleaned from multiline codeblock formatting.
-
-    Returns
-    -------
-    str
-        The cleaned string without multiline codeblock formatting.
-    """
-
-    matches = re.findall(MULTI_CODE_BLOCK_REGEX, content)
-    if not matches:
-        return content
-    cleaned = re.sub(f"```{matches[0]}```", matches[0], content)
-    for match in matches:
-        cleaned = re.sub(f"```{match}```", match, cleaned)
-    return cleaned
-
-
-def remove_bold(content: str) -> str:
-    """Removes the bold formatting from Discord messages.
-
-    Parameters
-    ----------
-    content : str
-        The `str` object to be cleaned from bold formatting.
-
-    Returns
-    -------
-    str
-        The cleaned string without bold formatting.
-    """
-    matches = re.findall(BOLD_REGEX, content)
-    if not matches:
-        return content
-    cleaned = re.sub(f"\*\*{matches[0]}\*\*", matches[0], content)
-    for match in matches:
-        cleaned = re.sub(f"\*\*{match}\*\*", match, cleaned)
-    return cleaned
-
-
-def remove_underlines(content: str) -> str:
-    """Removes the underlining from Discord messages.
-
-    Parameters
-    ----------
-    content : str
-        The `str` object to be cleaned from underlining.
-
-    Returns
-    -------
-    str
-        The cleaned string without underlining.
-    """
-    matches = re.findall(UNDERLINE_REGEX, content)
-    if not matches:
-        return content
-    cleaned = re.sub(f"__{matches[0]}__", matches[0], content)
-    for match in matches:
-        cleaned = re.sub(f"__{match}__", match, cleaned)
-    return cleaned
-
-
-def remove_underscore_italics(content: str) -> str:
-    """Removes the italic formatting from Discord messages caused by single underscores.
-
-    Parameters
-    ----------
-    content : str
-        The `str` object to be cleaned from italic formatting.
-
-    Returns
-    -------
-    str
-        The cleaned string without italic formatting.
-    """
-    matches = re.findall(ITALIC_UNDERSCORE_REGEX, content)
-    if not matches:
-        return content
-    cleaned = re.sub(f"_{matches[0]}_", matches[0], content)
-    for match in matches:
-        cleaned = re.sub(f"_{match}_", match, cleaned)
-    return cleaned
-
-
-def remove_asterisk_italics(content: str) -> str:
-    """Removes the italic formatting from Discord messages caused by single asterisks.
-
-    Parameters
-    ----------
-    content : str
-        The `str` object to be cleaned from italic formatting.
-
-    Returns
-    -------
-    str
-        The cleaned string without italic formatting.
-    """
-    matches = re.findall(ITALIC_ASTERISK_REGEX, content)
-    if not matches:
-        return content
-    cleaned = re.sub(f"\*{matches[0]}\*", matches[0], content)
-    for match in matches:
-        cleaned = re.sub(f"\*{match}\*", match, cleaned)
-    return cleaned
-
-
-def remove_spoilers(content: str) -> str:
-    """Removes the spoiler from Discord messages.
-
-    Parameters
-    ----------
-    content : str
-        The `str` object to be cleaned from spoilers.
-
-    Returns
-    -------
-    str
-        The cleaned string without spoilers.
-    """
-    matches = re.findall(SPOILER_REGEX, content)
-    if not matches:
-        return content
-    cleaned = re.sub(f"\|\|{matches[0]}\|\|", matches[0], content)
-    for match in matches:
-        cleaned = re.sub(f"\|\|{match}\|\|", match, cleaned)
-    return cleaned
-
-
-def remove_quotes(content: str) -> str:
-    """Removes the quote formatting from Discord messages.
-
-    Parameters
-    ----------
-    content : str
-        The `str` object to be cleaned from quote formatting.
-
-    Returns
-    -------
-    str
-        The cleaned string without quote formatting.
-    """
-    matches = re.findall(QUOTE_REGEX, content)
-    if not matches:
-        return content
-    cleaned = re.sub(f"> {matches[0]}", matches[0], content)
-    for match in matches:
-        cleaned = re.sub(f"> {match}", match, cleaned)
-    return cleaned
-
-
-def remove_multi_quotes(content: str) -> str:
-    """Removes the multiline quote formatting from Discord messages.
-
-    Parameters
-    ----------
-    content : str
-        The `str` object to be cleaned from multiline quote formatting.
-
-    Returns
-    -------
-    str
-        The cleaned string without multiline quote formatting.
-    """
-    matches = re.findall(MULTI_QUOTE_REGEX, content)
-    if not matches:
-        return content
-    cleaned = re.sub(f">>> {matches[0]}", matches[0], content)
-    for match in matches:
-        cleaned = re.sub(f">>> {match}", match, cleaned)
-    return cleaned
 # MIT License
 #
 # Copyright (c) 2022-present HyperGH
