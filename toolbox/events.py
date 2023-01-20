2c00a966a73f98069a7579b1ab98add547a2d2d4
Lunarmagpie <bambolambo0@gmail.com>
2023-01-20 14:41:21 -0500

    add event decorator

---
+++ toolbox/events.py
@@ -0,0 +1,34 @@
+import typing as t
+
+import hikari
+import functools
+
+__all__: t.Sequence[str] = ["consume_event"]
+
+P = t.ParamSpec("P")
+T = t.TypeVar("T")
+
+
+def consume_event(callback: t.Callable[P, T]) -> t.Callable[t.Concatenate[hikari.Event, P], T]:
+    """
+    Consume the first argument of an event callback.
+
+    .. code-block:: python
+
+        import hikari
+        import toolbox
+
+        @toolbox.consume_event
+        async def on_started():
+            ...
+
+        bot = hikari.GatewayBot("TOKEN")
+        bot.subscribe(hikari.StartingEvent, on_started)
+        bot.run()
+    """
+
+    @functools.wraps(callback)
+    async def inner(_: hikari.Event, *args: P.args, **kwargs: P.kwargs) -> T:
+        return await callback(*args, **kwargs)
+
+    return inner
